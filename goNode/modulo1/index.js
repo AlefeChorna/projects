082577769666a3e565420015f647a830f4203bf9
Aléfe Chorna <alefechorna99@gmail.com>
2018-11-30 21:31:56 -0200

    Created simple server with Express. Created simple examples of routes "GET" with Express. Created the file "gitignore".

--- goNode/modulo1/index.js
+++ goNode/modulo1/index.js
@@ -1,7 +1,36 @@
 const http = require('http')
+const express = require('express')
 
-http.createServer((req, res) => {
-    console.log(req)
+const app = express()
 
-    return res.end('Hello World')
-}).listen(3000)
+app.get('/', (req, res) => {
+    res.send('Hello Word with Express')
+})
+
+// Request: http://localhost:3000/name/Alefe
+// Response: Welcome Alefe
+app.get('/name/:name', (req, res) => {
+    return res.send(`Welcone ${req.params.name}`)
+})
+
+// Request: http://localhost:3000/age?age=20
+// Response: Your are 20 years old
+app.get('/age', (req, res) => {
+    return res.send(`Your are ${req.query.age} years old`)
+})
+
+// Request: http://localhost:3000/users
+// Response: [{"name":"John","age":21},{"name":"Alice","age":20}]
+app.get('/users', (req, res) => {
+    return res.json([
+        {
+            name: 'John',
+            age: 21
+        }, {
+            name: 'Alice',
+            age: 20
+        }
+    ])
+})
+
+app.listen(3000)
