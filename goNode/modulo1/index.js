2695d8c2defc72e1894575572e04f3c187cc7b15
Aléfe Chorna <alefechorna99@gmail.com>
2018-12-01 01:02:43 -0200

    Implemented so that it is possible to create a new user

--- goNode/modulo1/index.js
+++ goNode/modulo1/index.js
@@ -10,6 +10,8 @@ nunjucks.configure('views', {
     watch: true
 })
 
+app.use(express.urlencoded({ extended: false }))
+
 // Sets the extensions for nunjucks files to be "njk"
 app.set('view engine', 'njk')
 
@@ -20,9 +22,16 @@ app.get('/', (req, res) => {
     res.render(`list`, { users })
 })
 
-// Create new user
+// Create view new user
 app.get('/new', (req, res) => {
     return res.render('new')
+})
+
+// Create new User
+app.post('/create', (req, res) => {
+    users.push(req.body.name)
+
+    res.redirect('/')
 })
 
 app.listen(3000)
